f62bedfec751c7c90127e8ac592fefc709592599
Tom Johnson <tom@thebitcrusher.net>
2015-08-02 15:15:20 +1000

    vim base handler

--- config.sh
+++ config.sh
@@ -4,6 +4,10 @@
 # https://github.com/hertzz/dotfiles
 #
 
+# Override home directory location to
+# use something non-standard
+HOME_DIRECTORY="${BASE_DIR}/home_sandbox"
+
 # List of handlers to load for dotfile
 # linking at runtime
 HANDLERS_ENABLED=(vim)
